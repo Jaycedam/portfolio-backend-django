4e2121eb24bb2ddb3edfbded5fe18228e93bdf9a
Jordan <jaycedam@gmail.com>
2022-10-26 20:06:39 -0300

    added JWT auth

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,12 @@ from django.urls import path, include
 from .views import ProjectViewSet, AreaViewSet
 from rest_framework import routers
 
+# JWT Authentication Views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 # routes for the viewsets API
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet)
@@ -9,4 +15,6 @@ router.register('areas', AreaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
